0b2a3be3a903aefa49b5d692782ea7295f7985dd
pvillor <pvillor@gmail.com>
2024-04-02 23:10:35 -0300

    feat: create table attendees

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 import fastify from "fastify";
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
+import { createEvent } from "./routes/create-event";
 
 const app = fastify();
 
-app.get("/", () => {
-  return "Link Start";
-});
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(createEvent);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running");
