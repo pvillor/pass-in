23718e847ce489a14ebc0f155e8108a9a3f39067
paulo.guerra <paulo.guerra@flex-am.com.br>
2024-04-03 08:28:12 -0400

    feat: new routes

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,9 @@ import {
   validatorCompiler,
 } from "fastify-type-provider-zod";
 import { createEvent } from "./routes/create-event";
+import { registerForEvent } from "./routes/register-for-event";
+import { getEvent } from "./routes/get-event";
+import { getAttendeeBadge } from "./routes/get-attendee-badge";
 
 const app = fastify();
 
@@ -11,6 +14,9 @@ app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(createEvent);
+app.register(getEvent);
+app.register(registerForEvent);
+app.register(getAttendeeBadge);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running");
